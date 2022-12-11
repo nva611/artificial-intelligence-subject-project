b9060589fd444eb740e051ecb559cfa5620325dc
Nguyễn Văn An <nva6112002@gmail.com>
2022-12-11 11:58:55 +0700

    Fix module

--- test.py
+++ test.py
@@ -1,13 +1,3 @@
+from TrafficSignRecognize import traffic
 
-
-import os
-error = []
-for i in range(1, 4):
-    print("xoa FaceRecognize/aaaa/An Nguyen Van 1.4."+str(i)+".jpg")
-    try:
-        os.remove("FaceRecognize/aaaa/An Nguyen Van 1.4."+str(i)+".jpg")
-        continue
-
-    except WindowsError:
-        print("HUHU")
-        continue
+traffic.recognize()
